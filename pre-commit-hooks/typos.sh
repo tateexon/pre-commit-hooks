82f32fe666fb915416e3c6ee9809f0b3df4bf154
Tate Exon <tateexon@gmail.com>
2024-07-30 08:00:48 -0600

    fix glob issue

--- pre-commit-hooks/typos.sh
+++ pre-commit-hooks/typos.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
+# Exit immediately if a command exits with a non-zero status
+set -e
+
 # Function to check for typos
 check_typos() {
     local config_option=""
     if [[ -f "./_typos.toml" ]]; then
         config_option="--config ./_typos.toml"
     fi
-    typos "$config_option" --force-exclude "$@" .
+    # shellcheck disable=SC2086
+    typos $config_option --force-exclude "$@"
 }
 
 # Run the typos check
@@ -17,9 +21,9 @@ typos_result=$?
 if [[ $typos_result -ne 0 ]]; then
     echo -e "❌ Found typos\n"
     if [[ -f "./_typos.toml" ]]; then
-        echo -e "Run 'typos --write-changes --config ./_typos.toml --force-exclude $* .' and fix any issues left\n"
+        echo -e "Run 'typos --write-changes --config ./_typos.toml --force-exclude $*' and fix any issues left\n"
     else
-        echo -e "Run 'typos --write-changes --force-exclude $* .' and fix any issues left\n"
+        echo -e "Run 'typos --write-changes --force-exclude $*' and fix any issues left\n"
     fi
     exit 1
 fi
